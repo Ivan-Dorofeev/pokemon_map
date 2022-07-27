e28069df6c332d0397be220962f8861483186215
Ivan Dorofeev <axxel123@rambler.ru>
2022-07-27 08:12:36 +0300

    - Изменил обязательность полей моделей

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,8 +3,8 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField("Название", max_length=200)
-    photo = models.ImageField("Изображение", upload_to='pokemons', blank=True)
-    description = models.TextField("Описание", max_length=1000, blank=True)
+    photo = models.ImageField("Изображение", upload_to='pokemons')
+    description = models.TextField("Описание", max_length=1000)
     title_en = models.CharField("Название на ангийском", max_length=30, blank=True)
     title_jp = models.CharField("Название на японском", max_length=30, blank=True)
     previous_evolution = models.ForeignKey('self', related_name='pokemon_evolusions', on_delete=models.CASCADE,
@@ -16,8 +16,8 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, related_name='pokemons', on_delete=models.CASCADE)
-    lat = models.FloatField("Широта", null=True)
-    lon = models.FloatField("Долгота", null=True)
+    lat = models.FloatField("Широта")
+    lon = models.FloatField("Долгота")
     appeared_at = models.DateTimeField("Время появления", null=True)
     disappeared_at = models.DateTimeField("Время исчезновение", null=True)
     level = models.IntegerField("Уровень", default=0)
