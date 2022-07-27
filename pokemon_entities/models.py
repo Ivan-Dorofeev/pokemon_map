04e61487184b3aeb5b9ae393b209f731e6d6e035
Ivan Dorofeev <axxel123@rambler.ru>
2022-07-27 08:06:24 +0300

    - добавил related_name для ForeighKey

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -15,7 +15,7 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, related_name='Покемон', on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(Pokemon, related_name='pokemons', on_delete=models.CASCADE)
     lat = models.FloatField("Широта", null=True)
     lon = models.FloatField("Долгота", null=True)
     appeared_at = models.DateTimeField("Время появления", null=True)
